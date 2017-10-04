7d1e33c48a155935685213ac34c7a269803a1132
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2017-10-04 14:35:32 -0300

    update Register
    - update Register

--- register.php
+++ register.php
@@ -7,26 +7,30 @@ include_once 'src\funtions\db.php';
 include_once 'src\funtions\security.php';
 
 if(isset($_POST['nikenamePost']) AND isset($_POST['passwordPost']) AND isset($_POST['emailPost'])){
-    echo $_POST['nikenamePost']." -|- ".$_POST['passwordPost']." -|- ".$_POST['emailPost']."</br>";
+
     //make a key for encrypt
     $key = makeKey($_POST['nikenamePost'], $_POST['passwordPost']);
 
-    echo $key."</br>";
     //encrypt $_POST['nikenamePost'] and $_POST['passwordPost']
     $_POST['nikenamePost'] = encrypt($_POST['nikenamePost'], $key);
     $_POST['passwordPost'] = encrypt($_POST['passwordPost'], $key);
     $_POST['emailPost'] = encrypt($_POST['emailPost'], $key);
 
-    echo $_POST['nikenamePost']." -|- ".$_POST['passwordPost']." -|- ".$_POST['emailPost']."</br>";
     //start connection with db and return link
     $link = db_connection();
 
-    if ($link){
-      echo 'connection ok!</br>';
-    }else{
-      echo 'erro connection!</br>';
-    }
+    $nikename         = decrypt($_POST['nikenamePost'], $key);
+    $password         = $_POST['passwordPost'];
+    $name             = decrypt($_POST['namePost'], $key);
+    $email            = decrypt($_POST['mailPost'], $key);
+    $data_ts          = time( );
+    $user_active      = 0;
+    $sendMailExeption = $_POST['sendMailExeptionPost'];
+
+    $sql              = "INSERT INTO users(nikename,password,name,email,date_ts,user_active,sendMailExeption) ";
+    $sql              = ."VALUES('".$nikename."','".$password."','".$name."','".$email."','".$date_ts."','".$user_active."','".$sendMailExeption."')";
+    echo $sql;
     //query FROM users
-    $rs = mysqli_query($link, "INSERT INTO users(nikename,password,email) VALUES('". decrypt($_POST['nikenamePost'], $key) ."','". $_POST['passwordPost'] ."','". decrypt($_POST['emailPost'], $key) ."')");
+    $rs = mysqli_query($link, $sql);
 }
 ?>
